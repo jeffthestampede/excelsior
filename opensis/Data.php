c6896e5c11f657e3c663e2efa5144d3422170c9c
jeffthestampede <jeffthestampede@gmail.com>
2017-02-28 20:56:10 -0800

    remove opensis

--- opensis/Data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DatabaseType = 'mysqli'; 
-$DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'timothylee'; 
-$DatabasePassword = 'Sis@Manager'; 
-$DatabaseName = 'excelsior89_opensis'; 
-$DatabasePort = '3306'; 
-?>
